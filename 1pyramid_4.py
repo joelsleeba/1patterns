91df4cea87997b1dcd10c174c1b0d9d2d0aa4e7f
Joel Sleeba <54855387+joelsleeba1@users.noreply.github.com>
2020-01-22 16:33:03 +0530

    Add files via upload

---
+++ 1pyramid_4.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    print(" "*2*(4-i), end='')
+    for j in range(i+1):
+        print("1", end=" ")
+    print("")
+
+
+#output
+#        1
+#      1 1
+#    1 1 1
+#  1 1 1 1
+#1 1 1 1 1
---
+++ 1pyramid_5.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    for j in range(i+1,0):
+        print("")
+#output
+#1 1 1 1 1
+#  1 1 1 1
+#    1 1 1
+#      1 1
+#        1
